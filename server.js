67b08b1721aad284baa98204ca410403bc665bb4
Sampada Thapa <sampadathapa2020@gmail.com>
2025-07-19 20:21:39 +0545

    3 complete

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
 
 //configure env
 dotenv.config();
 
+//database
+connectDB();
+
 //rest object
 const app = express();
 
+//middlewares
+app.use(express.json()); //to accept json data
+app.use(morgan('dev')); //to log requests in development mode
+
 //rest api
 app.get('/', (req, res) => {
-  console.log("Homepage route accessed");
-  res.send('<h1 style="color: red;">✨ Updated ESSENZA Homepage ✨</h1>');
+  res.send('<h1>ESSENZA Homepage</h1>');
 });
 
 //secure port
